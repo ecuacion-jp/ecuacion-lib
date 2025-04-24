2d337929a1a900f9d91269b6bb15393cb9c30feb
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2025-04-24 13:56:20 +0900

    #271: spotbugs fixed.

--- ecuacion-lib-core/src/main/java/jp/ecuacion/lib/core/jakartavalidation/validator/internal/ConditionalValidator.java
+++ ecuacion-lib-core/src/main/java/jp/ecuacion/lib/core/jakartavalidation/validator/internal/ConditionalValidator.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright © 2012 ecuacion.jp (info@ecuacion.jp)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package jp.ecuacion.lib.core.jakartavalidation.validator.internal;
 
 import jakarta.validation.ConstraintValidatorContext;
-import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import jp.ecuacion.lib.core.constant.EclibCoreConstants;
 import jp.ecuacion.lib.core.exception.unchecked.EclibRuntimeException;
@@ -114,23 +129,25 @@ public abstract class ConditionalValidator {
   }
 
   public static Object getFieldValue(String fieldName, Object instance, String fieldKindName) {
-    Field validationTargetField;
-
+    Method validationTargetFieldGetterMethod;
+    String methodName = "get" + StringUtils.capitalize(fieldName);
+    
     try {
-      validationTargetField = instance.getClass().getDeclaredField(fieldName);
-      validationTargetField.setAccessible(true);
-
-    } catch (Exception ex) {
-      throw new EclibRuntimeException("'" + fieldKindName + "' field instance cannot be obtained "
-          + "from the field name '" + fieldName + "'", ex);
-    }
-
-    try {
-      return validationTargetField.get(instance);
+      validationTargetFieldGetterMethod = instance.getClass().getMethod(methodName);
 
     } catch (Exception ex) {
       throw new EclibRuntimeException(
-          "The value of '" + fieldName + "' instance cannot be obtained.", ex);
+          "'" + fieldKindName + "' getter method instance cannot be obtained "
+              + "from the method name '" + methodName + "'",
+          ex);
+    }
+
+    try {
+      return validationTargetFieldGetterMethod.invoke(instance);
+
+    } catch (Exception ex) {
+      throw new EclibRuntimeException(
+          "The return value of '" + methodName + "' instance cannot be obtained.", ex);
     }
   }
 
