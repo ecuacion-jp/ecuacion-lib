14d906aab575c99fe1aa7712a7b71eddd42d8243
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2025-05-24 17:25:42 +0900

    Comments in annotations updated.

--- ecuacion-lib-core/src/main/java/jp/ecuacion/lib/core/annotation/RequireSizeNonzero.java
+++ ecuacion-lib-core/src/main/java/jp/ecuacion/lib/core/annotation/RequireSizeNonzero.java
@@ -21,14 +21,11 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 
 /**
- * Designates that the method throws RequireNonEmptyException 
- *     when the annotated arguments is empty. (= null or blank(""))
+ * Designates that the method throws RequireSizeNonZeroException 
+ *     when the length of the annotated list or array is zero.
  * 
  * <p>This annnotation does nothing. 
  *     Just for the explanation of the implementation of the method to developers.</p>
- *
- * @see jakarta.annotation.Nonnull
- * @see java.util.Objects
  */
 @Documented
 @Retention(RUNTIME)
