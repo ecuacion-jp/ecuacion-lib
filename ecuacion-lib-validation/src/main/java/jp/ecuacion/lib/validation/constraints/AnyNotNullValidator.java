6d63e06cff1e5ca0d5bff4741eb3bad66e00eeab
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2026-04-09 08:22:19 +0900

    Addition of validators
    - @NullWhen, @NotNullWhen
    - @AnyNull, @AnyNotNull, @AllNullOrAllNotNull

---
+++ ecuacion-lib-validation/src/main/java/jp/ecuacion/lib/validation/constraints/AnyNotNullValidator.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright © 2012 ecuacion.jp (info@ecuacion.jp)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.ecuacion.lib.validation.constraints;
+
+import jakarta.validation.ConstraintValidatorContext;
+import jp.ecuacion.lib.validation.constraints.internal.AllAnyValidator;
+
+/**
+ * Provides the validation logic for {@code AnyNotNull}.
+ */
+public class AnyNotNullValidator extends AllAnyValidator<AnyNotNull, Object> {
+
+  /** Initializes an instance. */
+  @Override
+  public void initialize(AnyNotNull annotation) {
+    super.initialize(annotation.message(), annotation.propertyPath());
+  }
+
+  @Override
+  public boolean internalIsValid(Object object, ConstraintValidatorContext context) {
+    return numberOfNonNullValues(object) > 0;
+  }
+}
---
+++ ecuacion-lib-validation/src/main/java/jp/ecuacion/lib/validation/constraints/AnyNull.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright © 2012 ecuacion.jp (info@ecuacion.jp)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.ecuacion.lib.validation.constraints;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import jp.ecuacion.lib.core.jakartavalidation.annotation.PlacedAtClass;
+import jp.ecuacion.lib.validation.constraints.AnyNull.AnyNullList;
+
+/**
+ * Is valid when at least one of the values of {@code propertyPath} is null.
+ */
+@PlacedAtClass
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Repeatable(AnyNullList.class)
+@Documented
+@Constraint(validatedBy = {AnyNullValidator.class})
+public @interface AnyNull {
+
+  /**
+   * Is the array of propertyPath.
+   * The validation result is true when one of the values is null.
+   */
+  String[] propertyPath();
+
+  /**
+   * Returns message ID.
+   */
+  String message() default "{jp.ecuacion.lib.validation.constraints.AnyNull.message}";
+
+  /**
+   * Returns groups.
+   */
+  Class<?>[] groups() default {};
+
+  /**
+   * Returns payload.
+   */
+  Class<? extends Payload>[] payload() default {};
+
+  /**
+   * Defines several {@link AnyNull} annotations on the same element.
+   */
+  @Target({ElementType.TYPE})
+  @Retention(RetentionPolicy.RUNTIME)
+  @Documented
+  public @interface AnyNullList {
+
+    /**
+     * Returns an array of {@link AnyNull}.
+     */
+    AnyNull[] value();
+  }
+}
