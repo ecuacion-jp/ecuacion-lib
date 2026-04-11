699774806d97e37a4d6699ab3953d92beef61393
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2026-04-11 15:37:55 +0900

    bugfixes of Item getting procedures

--- ecuacion-lib-core/src/test/java/jp/ecuacion/lib/core/util/ExceptionUtilTest.java
+++ ecuacion-lib-core/src/test/java/jp/ecuacion/lib/core/util/ExceptionUtilTest.java
@@ -90,9 +90,6 @@ public class ExceptionUtilTest {
   public void getMessageList_variousPlacesTest() {
     String message = null;
 
-    // warning becuase "myChildList[0].name" is not right.
-    String test = null;
-    
     // normal
     message = getMsg(new VariousPlaces.Normal(null));
     Assertions.assertEquals("'normal.name' must not be null.", message);
@@ -101,7 +98,7 @@ public class ExceptionUtilTest {
     Assertions.assertEquals("'myChild.name' must not be null.", message);
     // inside child node in list
     message = getMsg(new VariousPlaces.InsideChildNodeInList(List.of(new Child(null))));
-    Assertions.assertEquals("'myChildList[0].name' must not be null.", message);
+    Assertions.assertEquals("'child.name' must not be null.", message);
     // anonymous class
     message = getMsg(new VariousPlaces.AnonymousClass());
     Assertions.assertEquals("'myChild.name' must not be null.", message);
